74b364b0cb18dfeb0e0998d30320bda4f405fe6b
Rabbott8 <rabbott8@students.d125.org>
2016-03-10 08:45:14 -0600

    Finished Deck.java and pushing changes

--- Deck.java
+++ Deck.java
@@ -15,12 +15,12 @@ to sort and a similar algorithm to shuffle.  Use Math.random().
 
  */
 import java.util.List;
+import java.util.Collections;
 import java.util.*;
-
-ArrayList<Card> Deck;
 
 public class Deck
 {
+    ArrayList<Card> Deck;
     //----------------------------------------------------
     // Constructs an empty deck of cards
     //----------------------------------------------------
@@ -34,5 +34,66 @@ public class Deck
     public Deck(int capacity)
     {
         Deck = new ArrayList<Card>(capacity);
+    }
+    //----------------------------------------------------
+    // Returns the number of cards in this deck.
+    //----------------------------------------------------
+    public int getNumCards()
+    {
+        return Deck.size();
+    }
+    //----------------------------------------------------
+    // Adds a given card at the top of this deck.
+    //----------------------------------------------------
+    public void add(Card card)
+    {
+        Deck.add(card);
+    }
+    //----------------------------------------------------
+    // Indicates whether this deck is empty.
+    //----------------------------------------------------
+    public boolean isEmpty()
+    {
+        if (Deck.size() == 0)
+            return true;
+        else
+            return false;
+    }
+    //----------------------------------------------------
+    // Removes and returns the top card from this deck.
+    //----------------------------------------------------
+    public Card takeTop()
+    {
+        //Note: The last element of the array is considered to be the top of the deck
+        if (!isEmpty())
+            return (Deck.remove(Deck.size()-1));
+        else
+            return null;
+    }
+    //----------------------------------------------------
+    // Shuffles this deck.
+    //----------------------------------------------------
+    public void shuffle()
+    {
+        Collections.shuffle(Deck);
+    }
+    //----------------------------------------------------
+    // Sorts this deck in acsending order of IDs.
+    //----------------------------------------------------
+    public void sort()
+    {
+        Collections.sort(Deck);
+    }
+    //----------------------------------------------------
+    // Returns a string representation of this deck.
+    //----------------------------------------------------
+    public String toString()
+    {
+        String s = "";
+        for (int i = 0; i<Deck.size(); i++)
+            {
+                s+=Deck.get(i) + " ";
+            }
+        return s;
     }
 }
