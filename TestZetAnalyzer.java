1efd8c35f24b4d60a6ee1c8b3fad64af4f74b705
Akhil Aggarwal 2018 <aaggarw8@students.d125.org>
2016-03-11 08:41:24 -0600

    finished testzetanalyzer

--- TestZetAnalyzer.java
+++ TestZetAnalyzer.java
@@ -1,17 +1,23 @@
 public class TestZetAnalyzer
 {
     
-    private int x;
-    public TestZetAnalyzer()
+    
+    public static void main (String [] args)
     {
-        // initialise instance variables
-        x = 0;
-    }
+        ZetDeck z1 = new ZetDeck();
+        int length = z1.length() - 1;
+        for (int i = 0; i < length; i++)
+        {
+            System.out.println(z1[i]);
+        }
+        
+        if (z1.findZet(z1) != null)
+        {
 
-
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+            System.out.println(findZet(z1));
+      
+    
+        
+        }
     }
 }
